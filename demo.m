325b58c434512ffed9b7d76e160ea183ba1c7c96
任毅 <tonghelen@126.com>
2018-05-26 14:30:27 +0800

    Delete demo.m

--- demo.m
+++
@@ -1,6 +0,0 @@
-for i = 1:7
-    scale = 1;     
-    img = double(imresize(imread(['./data/' num2str(i) '.bmp']),scale));
-    [I, R, L] = Ren(img);  
-    imwrite(I, ['./' num2str(i) '_Ren.bmp']); 
-end
